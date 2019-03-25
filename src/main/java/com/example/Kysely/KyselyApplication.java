e03136209a99b70bafcb8e51502321003ed7cf6c
Kuutok Mark <a1600507@myy.haaga-helia.fi>
2019-03-25 10:12:09 +0200

    projektin alustus

---
+++ src/main/java/com/example/Kysely/KyselyApplication.java
@@ -0,0 +1,14 @@
+package com.example.Kysely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselyApplication.class, args);
+		
+	}
+
+}
